42023058c9493edc6f4011c7982a9965fc889225
ayang <13264000065@163.com>
2018-10-24 14:40:34 +0800

    我的博客

--- data/config.cache.bak.php
+++ data/config.cache.bak.php
@@ -1,7 +1,7 @@
 <?php
-$cfg_basehost = 'http://web12';
+$cfg_basehost = 'http://localhost';
 $cfg_cmspath = '';
-$cfg_cookie_encode = 'L2V3k7FdOw379O4MtPyNITBKcnEQ';
+$cfg_cookie_encode = 'pt3mM4CfUePCRMSm9QpnOGmUKNoM';
 $cfg_indexurl = '/';
 $cfg_backup_dir = 'backupdata';
 $cfg_indexname = '主页';

60a632d6a9dbbc42a616cf95aaf7d743566c454e
ayang <13264000065@163.com>
2018-10-25 16:16:58 +0800

    在线翻译

---
+++ fanyi/src/components/TLoutput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="tloutput">
+    <!-- //调用 -->
+    <h2>{{tldtext}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tloutput',
+  //接受传过来的内容
+  props:[
+    "tldtext"
+  ]
+}
+</script>
+
+<style>
+
+</style>
